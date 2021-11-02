ee0ff7dbbe21574437d4ef9e8d24f3534a41d85f
ApprioryRoR <nickolayyu@wizardsdev.com>
2021-11-02 16:03:03 +0200

    added send message logic, added UserMessage component

--- src/sagas/firebaseSaga/chatSaga.js
+++ src/sagas/firebaseSaga/chatSaga.js
@@ -1,5 +1,13 @@
-import { apply } from 'redux-saga/effects';
+import { all, call, takeEvery } from 'redux-saga/effects';
+import * as chatMethods from '../../firebase/chatMethods'
+import actionTypes from '../../constants/actionTypes';
 
 export default function* watchChatSaga() {
-    yield apply(console, console.log, ['watchChatSaga'])
+    yield all([
+        takeEvery(actionTypes.SEND_MESSAGE, sendMessages),
+    ]);
 };
+
+export function* sendMessages({ payload }) {
+    yield call(chatMethods.sendMessageRequest, payload);
+}
